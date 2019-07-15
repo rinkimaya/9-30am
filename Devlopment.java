9b0ead1ce0ad7cb6415216d96c58d9648f25ee80
Raji1994 <rajcute222@gmail.com>
2019-07-15 09:27:42 -0700

    sample

---
+++ Devlopment.java
@@ -0,0 +1,21 @@
+package org.tcs;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Devlopment {
+	public static void main(String[] args) {
+		List<Integer> ex=new ArrayList<Integer>();
+		ex.add(10);
+		ex.add(20);
+		ex.add(30);
+		ex.add(40);
+		ex.add(50);
+		int x=ex.indexOf(50);
+		System.out.println(x);
+		
+	}
+	
+
+}
---
+++ Dummy.java
@@ -0,0 +1,31 @@
+package org.tcs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dummy {
+	public static void main(String[] args) {
+		
+	List<Integer> a=new ArrayList<Integer>();
+		a.add(10);
+	a.add(20);
+		a.add(3);
+	
+		a.add(40);
+		a.add(50);
+		a.add(60);
+	
+	System.out.println(a);
+	a.set(2,30);
+	System.out.println(a);
+		}
+			
+	
+		
+	}
+		
+	
+		
+		
+
+
